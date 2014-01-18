9dda796e8e82980cde2a33d6109bee4768abb9f8
Austin Keeley <austin.keeley@gmail.com>
2014-01-18 17:56:39 -0500

    Adding reservations saving

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,20 +1,22 @@
-class RegistrationsController < ApplicationController
+class ReservationsController < ApplicationController
   
+  # Save a new reservation
   def create
-    @reservation = Reservation.new
+    @reservation = Reservation.new(reservation_params)
     
+    @group = StudyGroup.find(reservation_params[:study_group_id])
+
     # Get the user from the session
     user_id = session[:user_id]
     @user = User.find(user_id)
-    reservation.user = @user
-    
-    group_id = params[:group_id]
-    @group = StudyGroup.find(group_id)
-
-    @reservation.group = @group
     @reservation.user = @user
+    @reservation.study_group = @group
     @reservation.save
 
+  end
+
+  def reservation_params
+    params[:reservation]
   end
 
 end
