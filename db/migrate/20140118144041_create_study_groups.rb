5a35797532b8765855d51d28ebd14b236fc3f3f4
Austin Keeley <austin.keeley@gmail.com>
2014-01-18 10:01:46 -0500

    Whups forgot that

--- db/migrate/20140118144041_create_study_groups.rb
+++ db/migrate/20140118144041_create_study_groups.rb
@@ -2,6 +2,10 @@ class CreateStudyGroups < ActiveRecord::Migration
   def change
     create_table :study_groups do |t|
 
+      t.string :name
+      t.date :start_date
+      t.date :end_date
+
       t.timestamps
     end
   end
