1dd5664ba9c9f2f36e47ba0f04122091ccb2b323
Austin Keeley <austin.keeley@gmail.com>
2014-01-18 16:58:33 -0500

    fade in

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,20 @@
 class RegistrationsController < ApplicationController
   
-  def new
+  def create
+    @reservation = Reservation.new
+    
+    # Get the user from the session
+    user_id = session[:user_id]
+    @user = User.find(user_id)
+    reservation.user = @user
+    
+    group_id = params[:group_id]
+    @group = StudyGroup.find(group_id)
+
+    @reservation.group = @group
+    @reservation.user = @user
+    @reservation.save
+
   end
 
 end
