027dedc683b5bffe1836a02648c4f95160975bee
Johnson <johnsonhu@gmail.com>
2014-01-18 12:07:01 -0500

    initial checkin for login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def title
+        base_title = "Collect<div>"
+        if @title.nil?
+        base_title
+    else
+        "#{base_title} | #{@title}"
+    end
+    end
+    
+    def logo
+        image_tag("logo.png", :alt => "CollectDiv", :class => "round")
+    end
 end
