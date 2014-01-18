5122443d94dda358416b4bbec4d97ad1b0492417
Austin Keeley <austin.keeley@gmail.com>
2014-01-18 09:04:15 -0500

    Adding user model

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ActiveRecord::Base
+
+  attr_accessor :password
+  attr_accessible :username, :email, :password, :password_confirmation, :active
+
+  before_save :encrypt_password
+
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+
+
+  validates :email, :presence => true,
+    :uniqueness => true
+
+  validates :username, :presence => true,
+    :uniqueness => true
+
+  # Authenticate a user against a password. Returns the user object if the user is authenticated
+  def self.authenticate(username, password)
+    user = find_by_username(username)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      return user
+    else
+      return nil
+    end
+  end
+
+  # Encrypts the password. This is done before saving the user to the database
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CollectdivWeb::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
