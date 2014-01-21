d199c8e39252b265b5ae2036e096616cc0b1065a
Johnson <johnsonhu@gmail.com>
2014-01-21 12:46:54 -0500

    testing my ssh 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-    #test comment
   # GET /users
   # GET /users.json
   def index
